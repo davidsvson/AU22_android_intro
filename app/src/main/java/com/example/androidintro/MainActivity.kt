5d0f012b86e92ccd28d4aa63967fd72e32f7fe52
David Svensson <davidsvson@gmail.com>
2022-09-13 13:33:54 +0200

    editText

--- app/src/main/java/com/example/androidintro/MainActivity.kt
+++ app/src/main/java/com/example/androidintro/MainActivity.kt
@@ -5,17 +5,20 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var textView : TextView
+    lateinit var inputView : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textView = findViewById(R.id.nameView)
+        inputView = findViewById(R.id.inputView)
 
         textView.text = "Susan"
 
@@ -26,8 +29,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun buttonPressed(view : View) {
+        val name = inputView.text.toString()
+
+        textView.text = "Hej $name!!"
         Log.d("!!!", "Knappen trycktes")
     }
 
 
 }
+
+
+
+
+
+
+
+
+
